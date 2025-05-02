7b61326883d1e57a1d05f689d092578715619389
Ilia Kholopkin <iliagold@icloud.com>
2025-05-02 13:19:21 +0300

    data validation and saving to localstorage for regular reminder page

--- WebApp/habits/new_negative_habit/step_1/positive_instead_negative/positive_instead_negative.js
+++ WebApp/habits/new_negative_habit/step_1/positive_instead_negative/positive_instead_negative.js
@@ -1,15 +1,14 @@
 import {mobile_focus_for_fields} from "../../../../tools/mobile_adaptations.js";
 import {send_data_to_server, send_page_name_to_server} from "../../../../tools/networking_tools.js";
 import {
-    create_input_date_and_time_fields, create_input_date_and_time_fields_holder,
+    create_input_date_and_time_fields,
     take_dates_and_times_from_page
 } from "../../../../tools/graphical_tools.js";
 import {
     get_item,
     serve_input_field,
     serve_accept_button,
-    on_accept_button,
-    off_accept_button, is_valid_time, set_item
+    recovery_for_days_and_times, validate_and_save_for_days_and_times
 } from "../../../../tools/auxiliary_tools.js";
 
 const accept_button = document.querySelector(".accept_button_div");
@@ -24,67 +23,12 @@ send_page_name_to_server("new_negative_habit/step_1/positive_instead_negative/po
 
 create_input_date_and_time_fields("../../../../icons/delete_inactive.svg");
 
-let loaded_days_and_times = JSON.parse(get_item("days_and_times"));
+recovery_for_days_and_times(input_fields_holder);
 
-if (loaded_days_and_times) {
-    for (let i = 0; i < loaded_days_and_times.days_of_week.length; ++i) {
-        create_input_date_and_time_fields_holder(input_fields_holder, "../../../../icons/delete_inactive.svg");
-    }
-
-    let now_time_input_elements = input_fields_holder.querySelectorAll(".input_data_and_time_fields_holder");
-
-    let all_ok = true;
-
-    for (let i = 0; i < loaded_days_and_times.days_of_week.length; ++i) {
-        now_time_input_elements[i].querySelector(".date_input_field").value = loaded_days_and_times.days_of_week[i];
-        now_time_input_elements[i].querySelector(".time_input_field").value = loaded_days_and_times.times[i];
-
-        if (!is_valid_time(loaded_days_and_times.times[i])) {
-            all_ok = false;
-        }
-    }
-
-    if (all_ok) {
-        on_accept_button("time", ["active", "time"]);
-    }
-}
-
-function validate_and_save() {
-    const now_time_input_elements = input_fields_holder.querySelectorAll(".time_input_field");
-    let all_ok = true;
-
-    const days_and_times = take_dates_and_times_from_page();
-    set_item("days_and_times", JSON.stringify(days_and_times));
-
-    if (now_time_input_elements.length === 1) {
-        all_ok = false;
-    } else {
-        for (let elem of now_time_input_elements) {
-            if (elem.value && !is_valid_time(elem.value)) {
-                all_ok = false;
-                break;
-            }
-        }
-
-        const has_any_text = Array.from(now_time_input_elements)
-            .some(el => el.value.trim() !== "");
-        if (!has_any_text) {
-            all_ok = false;
-        }
-    }
-
-    if (all_ok) {
-        on_accept_button("time", ["active", "time"]);
-    } else {
-        off_accept_button("time", ["active", "time"]);
-    }
-}
-
-input_fields_holder.addEventListener("input", validate_and_save);
-
+input_fields_holder.addEventListener("input", () => validate_and_save_for_days_and_times(input_fields_holder));
 input_fields_holder.addEventListener("click", (e) => {
     if (e.target.classList.contains("delete_active_svg")) {
-        validate_and_save();
+        validate_and_save_for_days_and_times(input_fields_holder);
     }
 });
 
--- WebApp/reminders/new_regular_reminder/new_regular_reminder.js
+++ WebApp/reminders/new_regular_reminder/new_regular_reminder.js
@@ -4,27 +4,54 @@ import {
     create_input_date_and_time_fields,
     take_dates_and_times_from_page
 } from "../../tools/graphical_tools.js";
-
+import {
+    recovery_for_days_and_times, remove_item,
+    serve_accept_button, serve_input_field,
+    validate_and_save_for_days_and_times
+} from "../../tools/auxiliary_tools.js";
 
 const accept_button = document.querySelector(".accept_button_div");
 const reminder_input_field = document.querySelector(".input_field");
+const input_fields_holder = document.querySelector(".input_fields_holder");
 
-create_input_date_and_time_fields("../../icons/delete_inactive.svg");
-accept_button.addEventListener("click", () => {
-    let days_and_times = take_dates_and_times_from_page();
+serve_input_field(reminder_input_field, "regular_reminder_text", false);
 
-    const url = "http://127.0.0.1:9091/new_regular_reminder";
-    let data_for_send = {
-        "text": reminder_input_field.value,
-        "dates": days_and_times.days_of_week,
-        "times": days_and_times.times,
-        // "tg_user_id": window.Telegram.WebApp.initDataUnsafe.user.id.toString()
-        "tg_user_id": "487020656"
+create_input_date_and_time_fields("../../../../icons/delete_inactive.svg");
+
+recovery_for_days_and_times(input_fields_holder, false);
+
+input_fields_holder.addEventListener("input", () => validate_and_save_for_days_and_times(input_fields_holder, false));
+input_fields_holder.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete_active_svg")) {
+        validate_and_save_for_days_and_times(input_fields_holder, false)
     }
-
-    send_data_to_server(url, data_for_send).then(r => {
-
-    });
 });
+
+accept_button.addEventListener("click", (event) => {
+    if (accept_button.getAttribute("active") === "true" &&
+        accept_button.getAttribute("time") === "true") {
+        event.preventDefault();
+
+        remove_item("regular_reminder_text");
+        remove_item("days_and_times");
+
+        let days_and_times = take_dates_and_times_from_page();
+
+        const url = "http://127.0.0.1:9091/new_regular_reminder";
+        let data_for_send = {
+            "text": reminder_input_field.value,
+            "dates": days_and_times.days_of_week,
+            "times": days_and_times.times,
+            // "tg_user_id": window.Telegram.WebApp.initDataUnsafe.user.id.toString()
+            "tg_user_id": "487020656"
+        }
+
+        send_data_to_server(url, data_for_send).then(r => {
+            window.location.href = "../reminders_page/reminders.html";
+        });
+    }
+});
+
+serve_accept_button([reminder_input_field], ["active", "time"]);
 
 mobile_focus_for_fields()
